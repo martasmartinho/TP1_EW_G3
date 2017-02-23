0335b2e793d1d21314dfa4d51384f8a155c6e90d
mmartinho1977 <mmartinho@ipca.pt>
2017-02-23 14:59:05 +0000

    XXXX

--- Server/BusinessLogic/client.rb
+++ Server/BusinessLogic/client.rb
@@ -1,5 +1,26 @@
-load 'Server/DataAccess/data_engine'
+
 
 class Client
 
+  def initialize(breed, name)
+    # Instance variables
+    @breed = breed
+    @name = name
+  end
+
+  def bark
+    puts 'Ruff! Ruff!'
+  end
+
+  def display
+    puts "I am of #{@breed} breed and my name is #{@name}"
+  end
+
 end
+
+d = Dog.new('Labrador', 'Benzy')
+
+
+puts "The id of d is #{d.object_id}."
+
+
--- Server/DataAccess/data_engine.rb
+++ Server/DataAccess/data_engine.rb
@@ -1,2 +1,3 @@
 class DataEngine
+
 end
