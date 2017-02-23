f31963fddca7d9080e7560e39c98f3b60996f530
mmartinho1977 <mmartinho@ipca.pt>
2017-02-23 18:49:10 +0000

    Logic layer first commit

--- Server/BusinessLogic/reading.rb
+++
@@ -1,5 +0,0 @@
-load 'Server/DataAccess/data_engine'
-
-class Reading
-
-end
--- Server/DataAccess/data_engine.rb
+++ Server/DataAccess/data_engine.rb
@@ -1,3 +1,34 @@
+
 class DataEngine
+
+  #Fields
+  attr_accessor :dataClient, :dataReading, :connectionString
+
+  #Constructor
+  def initialize(connectionString = "")
+
+      @connectionString = connectionString
+
+  end
+
+  #Get class properties
+  def getDataClient()
+
+    if (@dataClient == nill)
+      @dataClient = new.DataClient(this)
+    end
+
+    return @dataClient
+  end
+
+  def getDataReading()
+
+    if (@dataClient == nill)
+      @dataClient = new.DataClient(this)
+
+    end
+
+    return dataClient
+  end
 
 end
